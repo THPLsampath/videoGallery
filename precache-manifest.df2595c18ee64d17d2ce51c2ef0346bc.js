5e8fcd8358c1591564dce9330c14c780c3c956cf
sampath <sampaththpl@gmail.com>
2020-10-23 10:43:05 +0530

    Updates

---
+++ precache-manifest.df2595c18ee64d17d2ce51c2ef0346bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e554d4607b5f9da6a799a1ad533ba2",
+    "url": "/videoGallery/index.html"
+  },
+  {
+    "revision": "771b5ee7967561b261a0",
+    "url": "/videoGallery/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf86e7ca02903107a5d7",
+    "url": "/videoGallery/static/js/2.ed15c17d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/videoGallery/static/js/2.ed15c17d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771b5ee7967561b261a0",
+    "url": "/videoGallery/static/js/main.32bac3cc.chunk.js"
+  },
+  {
+    "revision": "e652550852313381f766",
+    "url": "/videoGallery/static/js/runtime-main.2924815f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videoGallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
